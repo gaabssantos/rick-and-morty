28995bb28ace7109132c8291827b4a6617e32414
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-08-25 12:58:47 -0300

    created name header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/header.component";
+import NameHeader from "./containers/NameHeader/name-header.container";
 
 function App() {
   return (
     <div>
       <Header />
+      <NameHeader />
       <h1>Hello!</h1>
     </div>
   );
---
+++ src/containers/NameHeader/name-header.container.tsx
@@ -0,0 +1,11 @@
+import styles from "./name-header.module.scss";
+
+const NameHeader = () => {
+  return (
+    <section className={styles.background}>
+      <h1>The Rick and Morty API</h1>
+    </section>
+  );
+};
+
+export default NameHeader;
